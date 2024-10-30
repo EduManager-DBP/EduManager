afb0850c91d9179cd6c9c04391038b0d8f464abe
sondahyun <sonda2356@naver.com>
2024-10-30 23:52:27 +0900

    init finish

--- EduManager/src/main/java/controller/RequestMapping.java
+++ EduManager/src/main/java/controller/RequestMapping.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import controller.user.*;
+//import controller.user.*;
 //import controller.comm.*;
 
 public class RequestMapping {
