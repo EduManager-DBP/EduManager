e3c6d6b884cce7e3b66527ae801a153f6bc2f699
sondahyun <sonda2356@naver.com>
2024-10-30 23:49:24 +0900

    init finish

--- EduManager/src/main/java/controller/RequestMapping.java
+++ EduManager/src/main/java/controller/RequestMapping.java
@@ -11,11 +11,11 @@ import controller.user.*;
 public class RequestMapping {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     
-    // 각 요청 uri에 대한 controller 객체를 저장할 HashMap 생성
+    // 각 요청 URI에 대한 controller 객체를 저장할 HashMap 생성
     private Map<String, Controller> mappings = new HashMap<String, Controller>();
 
     public void initMapping() {
-    	// 각 uri에 대응되는 controller 객체를 생성 및 저장
+    	// 각 URI에 대응되는 controller 객체를 생성 및 저장
         mappings.put("/", new ForwardController("index.jsp"));
         mappings.put("/user/login/form", new ForwardController("/user/loginForm.jsp"));
        
@@ -24,7 +24,7 @@ public class RequestMapping {
     }
 
     public Controller findController(String uri) {	
-    	// 주어진 uri에 대응되는 controller 객체를 찾아 반환
+    	// 주어진 URI에 대응되는 controller 객체를 찾아 반환
         return mappings.get(uri);
     }
 }
